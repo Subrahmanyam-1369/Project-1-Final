832170004e350d0115e4c7190586f979e7589c5a
Subrahmanyam-1369 <psubrahmanyam1369@gmail.com>
2025-03-27 12:08:56 +0000

    trying

--- deploy/cleanup_manager.sh
+++ deploy/cleanup_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd backups || exit
 ls -t | sed -e '1,10d' | xargs -d '\n' rm -rf --
-echo 🔄 Pulling latest changes... | tee -a logs/deploy.log
+echo 🔄 Pulling latest changes..... | tee -a logs/deploy.log
